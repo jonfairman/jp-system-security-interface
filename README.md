ed2d0b8123af31498f510f82def6910b18c01121
Jon Fairman <jonfairman@puppetlabs.com>
2016-08-21 17:28:29 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+Jurassic Park, System Security Interface
+========================================
+
+A weekend project for fun to teach myself some basic bash scripting.
+
+To interact with the interface, enter the following from the command line:
+
+```
+bash jp-system-security-interface.sh
+```
+
+You will be prompted to enter a command; from here you may enter:
+
+```
+access security
+```
+
+```
+access security grid
+```
+
+```
+access main security grid
+```
---
+++ jp-system-security-interface.sh
@@ -0,0 +1,33 @@
+# This script emulates the Jurassic Park security lockup script that Dennis Nedry created to lock everyone out of the system while he escaped... because, why not?
+#
+#!/bin/bash
+
+echo "Jurassic Park, System Security Interface"
+echo "Version 4.0.5, Alpha E"
+echo "Ready..."
+
+read -p "> "
+echo "access: PERMISSION DENIED."
+
+read -p "> "
+echo "access: PERMISSION DENIED."
+
+read -p "> "
+echo "access: PERMISSION DENIED....and..."
+
+qlmanage -p "./nedry.gif" >& /dev/null &
+
+for run in {1..42}
+do
+    echo -e "YOU DIDN'T SAY THE MAGIC WORD! \a"
+    sleep .5
+done &
+
+afplay ./you-didnt-say-the-magic-word.mp3
+afplay ./you-didnt-say-the-magic-word.mp3
+afplay ./you-didnt-say-the-magic-word.mp3
+afplay ./you-didnt-say-the-magic-word.mp3
+afplay ./you-didnt-say-the-magic-word.mp3
+afplay ./you-didnt-say-the-magic-word.mp3
+
+exit
